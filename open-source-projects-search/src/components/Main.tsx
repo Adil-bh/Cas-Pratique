c8c1549b61568f358bfb5c5bd8639ae524eae621
Anhpati <romain-felicianne@hotmail.fr>
2024-05-23 20:16:38 +0200

    ✨ F03 : MAIN > Add search form.

--- open-source-projects-search/src/components/Main.tsx
+++ open-source-projects-search/src/components/Main.tsx
@@ -4,15 +4,42 @@ export const Main = () => {
     return (
         <MainStyled>
             Main
+            <form action="">
+                <div>
+                    <div>
+                        <select name="technology-languages" id="technology-languages">
+                            <option value="javascript">Javascript</option>
+                            <option value="php">PHP</option>
+                            <option value="python">Python</option>
+                        </select>
+                    </div>
+                    <div>
+                        <select name="tags" id="tags">
+                            <option value="foot">foot</option>
+                            <option value="moto">moto</option>
+                            <option value="histoire">histoire</option>
+                        </select>
+                    </div>
+                    <div>
+                        <select name="spoken-languages" id="spoken-languages">
+                            <option value="français">Français</option>
+                            <option value="spanish">Spanish</option>
+                            <option value="english">English</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <button>Search</button>
+                </div>
+            </form>
         </MainStyled>
     )
 }
 
 const MainStyled = styled.main`
-    min-height: 100vh;
+    min-height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-  
 `;
