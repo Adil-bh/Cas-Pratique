1ecf6882284d3ad78803a250798a148fb1ded2d9
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-05-29 15:01:53 +0200

    💄 F05 : MAIN > Add Title component

--- open-source-projects-search/src/components/Main.tsx
+++ open-source-projects-search/src/components/Main.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import { SearchForm } from "./SearchForm/SearchForm";
+import { Title } from "./ui/Title";
 
 export const Main = () => {
     return (
         <MainStyled>
             Main
+            <Title />
             <SearchForm />
         </MainStyled>
     )
